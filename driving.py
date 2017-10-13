bcd208c9933353f3e9822d0d6f5eb911ef1b78c4
Jan <jangent@ut.ee>
2017-10-13 16:55:56 +0300

    newline problems

--- driving.py
+++ driving.py
@@ -9,11 +9,14 @@ wheel_angle3 = 180
 def set_speed(x, y, z):
     return
 
+
 def get_speed():
     return 0, 0, 0
 
+
 def rotation_time(deg, motor_speed):
     return 0.5
+
 
 def rotate_left(deg):
     motor_speed = 10
@@ -21,11 +24,13 @@ def rotate_left(deg):
     sleep(rotation_time(deg, motor_speed))
     set_speed(0, 0, 0)
 
+
 def rotate_right(deg):
     motor_speed = -10
     set_speed(motor_speed, motor_speed, motor_speed)
     sleep(rotation_time(deg, motor_speed))
     set_speed(0, 0, 0)
+
 
 def move(deg, dist):
     speed1 = robot_speed / math.cos(math.radians(wheel_angle1))
@@ -34,14 +39,19 @@ def move(deg, dist):
     set_speed(speed1, speed2, speed3)
     return 
 
+
 def move_forward(dist):
     move(0, dist)
     
+
 def move_left(dist):
     move(270, dist)
-    
+
+
 def move_right(dist):
     move(90, dist)
-    
+
+
 def move_backward(dist):
     move(180, dist)
+
