dde0dce638dcf00ccf778fb7813135b2396c9aba
Jan Aare van Gent <jan.aare.gent@estcube.eu>
2017-10-21 14:23:29 +0300

    used robot_movements module for movements

--- keyb_driving.py
+++ keyb_driving.py
@@ -1,44 +1,48 @@
-import serial
 import curses
+from robot_movements import *
 
 screen = curses.initscr()
-#ser = serial.Serial('/dev/ttyACM0')
 screen.keypad(True)
 curses.cbreak()
 curses.noecho()
-
-def set_speed(sp1, sp2, sp3):
-    #ser.write(str.encode('sd0:{}:{}:{}\n'.format(sp1, sp2, sp3)))
-    pass
+init_robot_connection()
 
 def process(c):
     if c == ord('w'):
         screen.addstr(0, 0, 'w')
+        set_speeds_for_direction(0)
     elif c == ord('a'):
         screen.addstr(0, 0, 'a')
+        set_speeds_for_direction(270)
     elif c == ord('s'):
         screen.addstr(0, 0, 's')
+        set_speeds_for_direction(180)
     elif c == ord('d'):
         screen.addstr(0, 0, 'd')
+        set_speeds_for_direction(90)
     elif c == ord('q'):
         screen.addstr(0, 0, 'q')
+        set_speeds_for_direction(315)
     elif c == ord('e'):
         screen.addstr(0, 0, 'e')
+        set_speeds_for_direction(45)
     elif c == ord('z'):
         screen.addstr(0, 0, 'z')
+        set_speeds_for_direction(225)
     elif c == ord('c'):
         screen.addstr(0, 0, 'c')
+        set_speeds_for_direction(135)
     elif c == curses.KEY_RIGHT:
         screen.addstr(0, 0, 'right')
-        set_speed(-10,-10,-10)
+        set_speeds(-10,-10,-10)
     elif c == curses.KEY_LEFT:
         screen.addstr(0, 0, 'left')
-        set_speed(10,10,10)
+        set_speeds(10,10,10)
     elif c == curses.KEY_UP:
         screen.addstr(0, 0, 'up')
     elif c == curses.KEY_DOWN:
         screen.addstr(0, 0, 'down')
-        set_speed(10, -10, 0)
+        set_speeds(10, -10, 0)
     else:
         pass
 
