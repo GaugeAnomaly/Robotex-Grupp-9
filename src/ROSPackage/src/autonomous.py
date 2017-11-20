3318ca4229ae46e0bb587ef75bbd0f42a7a61cf1
Jan <jangent@ut.ee>
2017-11-20 18:59:46 +0200

    Changed a lot of things since two weeks

--- src/ROSPackage/src/autonomous.py
+++ src/ROSPackage/src/autonomous.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 from time import sleep
 
 from robot_movements import *
@@ -16,32 +16,43 @@ ball_x = -1
 ball_y = -1
 basket_x = -1
 basket_y = -1
+basket_width = 0
 #  Booleans that get updated via callbacks
 ball_in_sight = False
 basket_in_sight = False
 ball_caught = False
 #  Maximum turning speed
-max_turn_speed = 30
-max_move_speed = 10
+max_turn_speed = 7
+max_move_speed = 7
 
 caught_lower_threshold = cam_height * 0.80
 ## Ball centering variables
-center_x = cam_width / 2
+offset = 10
+center_x = cam_width / 2 + offset
 center_y = cam_height / 2
-threshold_x1 = center_x - int(cam_width / 40)
-threshold_x2 = center_x + int(cam_width / 40)
+threshold_x1 = center_x - int(cam_width / 120) + offset
+threshold_x2 = center_x + int(cam_width / 120) + offset
+widths = [29,31,35,34,39,41,42,45,46,48,49,51,48,48.5,51,53.33,54,56,
+59.33, 61, 64.75, 67.333, 70, 74, 79.25, 81, 87.75, 95.6, 103]
+
+powers = [200,195,190,193,187,185,183,182,181,180,179,178,177,176,175,174,173,
+172,171,170,169,168,167,166,165,164,163,162,161]
 
 
 # Functions
 #  TODO: take into account the refereee start, ping and stop signals
 def referee_callback(data):
-    global allowed_to_play
+    global allowed_to_play, state, allowed_to_play
+    if data.data:
+        state = looking_for_ball
+    else:
+        allowed_to_play = False
     #  allowed_to_paly = data.data...
 
 
 # TODO: get basket information
 def cam_callback(data):
-    global ball_x, ball_y, ball_in_sight, basket_in_sight, basket_x, basket_y
+    global ball_x, ball_y, ball_in_sight, basket_in_sight, basket_x, basket_y, basket_width
     #rospy.loginfo(data.data)
     parsed_data = data.data.split("--")
     if parsed_data[0] == 'green':
@@ -64,6 +75,8 @@ def cam_callback(data):
         else:
             basket_x = float(parsed_data[1])
             basket_y = float(parsed_data[2])
+            basket_width = float(parsed_data[3])
+            #rospy.loginfo("basket width: %s", basket_width)
             basket_in_sight = True
 
 
@@ -74,10 +87,13 @@ def center_ball():
             stop_rotating()
         else:
             if ball_x < threshold_x1:
-                turn_left_state(min([(center_x - ball_x) * 0.20, max_turn_speed]))
+                turn_left_state(max_turn_speed)
+                # turn_left_state(max_turn_speed * (cam_height * 0.8 ball_y)
+                # turn_left_state(min([(center_x - ball_x) * 0.40, max_turn_speed]))
             elif ball_x > threshold_x2:
-                rospy.loginfo(min([(ball_x - center_x) * 0.20, max_turn_speed]))
-                turn_right_state(min([(ball_x - center_x) * 0.20, max_turn_speed]))
+                turn_right_state(max_turn_speed)
+                #rospy.loginfo(min([(ball_x - center_x) * 0.20, max_turn_speed]))
+                # turn_right_state(min([(ball_x - center_x) * 0.40, max_turn_speed]))
 
 
 # TODO: make moving proportional to offset
@@ -87,14 +103,18 @@ def center_basket():
             stop_moving()
         else:
             if basket_x < threshold_x1:
-                move_right_state(min([(center_x - basket_x) * 0.1, max_move_speed]))
+                move_right_state(min([(center_x - basket_x) * 0.2, max_move_speed]))
             elif basket_x > threshold_x2:
-                move_left_state(min([(basket_x - center_x) * 0.1, max_move_speed]))
+                move_left_state(min([(basket_x - center_x) * 0.2, max_move_speed]))
 
 
 # TODO: implement calculation
 def calculate_thrower_speed():
-    return 140
+    for i in range(len(widths)):
+        if widths[i] > basket_width + (basket_width * 0.1):
+            return powers[i]
+
+    #return 140 + 150 - basket_width
 
 
 # Predicate functions
@@ -111,22 +131,22 @@ def basket_is_centered():
 
 def transition_to_state(newstate):
     global state
-    #allowed_to_play = False
-    #rospy.loginfo(newstate)
+    allowed_to_play = False
+    rospy.loginfo(newstate)
     state = newstate
 
 
 # States
 #  TODO: add initial ball discovering rotation and maybe spatial mapping
 def looking_for_ball():
-    global state
     #rospy.loginfo("looking for ball state")
     #rospy.loginfo(ball_in_sight)
     if ball_in_sight:
         transition_to_state(moving_to_ball)
         #state = moving_to_ball
     else:
-        stop_moving()
+        # stop_moving()
+        turn_left_state(10)
 
 
 def moving_to_ball():
@@ -149,14 +169,22 @@ def moving_to_ball():
 # Assuming the ball is found, the robot will twirl around the ball until both the ball and the basket are centered
 def finding_basket():
     global state
+    rospy.loginfo("basket %d %d %d", threshold_x1, basket_x + offset, threshold_x2)
+    rospy.loginfo("ball %d %d %d", threshold_x1, ball_x + offset, threshold_x2)
+    # rospy.loginfo(ball_in_sight, basket_in_sight, ball_is_centered(), basket_is_centered())
     if ball_in_sight and not basket_in_sight:
+        # rospy.loginfo("basket %d %d %d", threshold_x1, basket_x + offset, threshold_x2)
+        # rospy.loginfo("ball %d %d %d", threshold_x1, ball_x + offset, threshold_x2)
         move_left_state(10)
         center_ball()
     elif ball_in_sight and basket_in_sight:
-        #rospy.loginfo(ball_is_centered())
         center_ball()
         center_basket()
+        # rospy.loginfo("basket %d %d %d", threshold_x1, basket_x + offset, threshold_x2)
+        # rospy.loginfo("ball %d %d %d", threshold_x1, ball_x + offset, threshold_x2)
         if ball_is_centered() and basket_is_centered():
+            # rospy.loginfo("basket %d %d %d", threshold_x1, basket_x + offset, threshold_x2)
+            # rospy.loginfo("ball %d %d %d", threshold_x1, ball_x + offset, threshold_x2)
             stop_moving()  # just in case
             stop_rotating()
             transition_to_state(throw_ball)
@@ -168,18 +196,39 @@ def throw_ball():
     global state
     #rospy.loginfo("thowing ball state")
     set_thrower_speed(calculate_thrower_speed())
-    move_forward_state(20)
+    move_forward_state(25)
     sleep(1)
     transition_to_state(looking_for_ball)
 
+# This should be the initial state
+def waiting_for_referee():
+    # rospy.loginfo(ball_is_centered())
+    #rospy.loginfo(ball_x)
+    #sleep(0.1)
+    pass
+
+# Only for testing
+def spinning_thrower():
+    set_thrower_speed(calculate_thrower_speed())
+    sleep(0.5)
+
+
+def twirl():
+    set_speeds(0,0,20)
 
 state = looking_for_ball
+# state = waiting_for_referee
 rospy.Subscriber("balldistance", String, cam_callback)
+rospy.Subscriber("referee", Bool, referee_callback)
 init_robot_connection()
 
+counter = 0
 while not rospy.is_shutdown() and allowed_to_play:
     sleep(0.01)
+    # rospy.loginfo("basket width: %d", basket_width)
     state()
+    #if counter % 200 == 0:
+        #rospy.loginfo("basket width: " + str(basket_width))
     #rospy.loginfo(state)
 
 deinit_robot_connection()
--- src/ROSPackage/src/orange_ball_detector.py
+++ src/ROSPackage/src/orange_ball_detector.py
@@ -5,8 +5,24 @@ import rospy
 from std_msgs.msg import String
 from time import sleep
 cap = cv2.VideoCapture(0)
+# cap.set(13, 0.6)
 pub = rospy.Publisher('balldistance', String, queue_size=2)
 rospy.init_node('orange_ball_detector')
+cam_width = 640
+cam_height = 480
+
+caught_lower_threshold = cam_height * 0.80
+## Ball centering variables
+offset = 10
+center_x = cam_width / 2 + offset
+center_y = cam_height / 2
+threshold_x1 = center_x - int(cam_width / 120) + offset
+threshold_x2 = center_x + int(cam_width / 120) + offset
+
+ball_threshold_low = int(cam_height * 19.5 / 24)
+ball_threshold_high = int(cam_height / 6)
+
+basket_threshold_low = int(cam_height * 2 / 6)
 
 def ballDistanceInfo(string):
     pub.publish(string)
@@ -20,35 +36,114 @@ def distanceBalls(color, mask, frame):
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        box = cv2.minAreaRect(c)
-        box = cv2.boxPoints(box)
-        (p1, p2, p3, p4) = box
-        width = abs(p1[0] - p2[0])
-        rospy.loginfo(width)
-        for (x, y) in box:
-		          cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
-        M = cv2.moments(c)
-        try:
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            # only proceed if the radius meets a minimum size
-            #rospy.loginfo(index % 10)
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, center, int(radius),
-                           (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                ballDistanceInfo(color+"--"+str(x)+"--"+str(y)+"--"+str(width))
-            else:
-                ballDistanceInfo(color+"--"+str(-1)+"--"+str(-1)+"--"+str(-1))
-        except ZeroDivisionError:
-            pass
+        # c = max(cnts, key=cv2.contourArea)
+        c = None
+        center = None
+        m_area = 0
+        for cnt in cnts:
+            area = cv2.contourArea(cnt)
+            moms = cv2.moments(cnt)
+            try:
+                cent = (int(moms["m10"] / moms["m00"]), int(moms["m01"] / moms["m00"]))
+                if area >= m_area and cent[1] > ball_threshold_high and cent[1] < ball_threshold_low:
+                    m_area = area
+                    c = cnt
+                    center = cent
+            except ZeroDivisionError:
+                pass
+        if c is not None:
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            box = cv2.minAreaRect(c)
+            box = cv2.boxPoints(box)
+            try:
+                # only proceed if the radius meets a minimum size
+                #rospy.loginfo(index % 10)
+                if radius > 3:
+                    (p1, p2, p3, p4) = box
+                    width = max(abs(p1[0] - p2[0]), abs(p1[0] - p3[0]))
+                    # rospy.loginfo(width)
+                    #sorted(box, key=lambda s: s[1])
+                    #width = abs(box[0][1] - box[3][1])
+                    for (x, y) in box:
+            		          cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+                    #rospy.loginfo(width)
+                    # draw the circle and centroid on the frame,
+                    # then update the list of tracked points
+                    cv2.circle(frame, center, int(radius),
+                               (0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                    ballDistanceInfo(color+"--"+str(x)+"--"+str(y)+"--"+str(width))
+                else:
+                    ballDistanceInfo(color+"--"+str(-1)+"--"+str(-1)+"--"+str(-1))
+            except ZeroDivisionError:
+                pass
     else:
         ballDistanceInfo(color+"--"+str(-1)+"--"+str(-1)+"---1")
     return
 
+
+def distanceBaskets(color, mask, frame):
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        # c = max(cnts, key=cv2.contourArea)
+        c = None
+        center = None
+        m_area = 0
+        for cnt in cnts:
+            area = cv2.contourArea(cnt)
+            moms = cv2.moments(cnt)
+            try:
+                cent = (int(moms["m10"] / moms["m00"]), int(moms["m01"] / moms["m00"]))
+                if area >= m_area and cent[1] < basket_threshold_low:
+                    m_area = area
+                    c = cnt
+                    center = cent
+            except ZeroDivisionError:
+                pass
+        if c is not None:
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            box = cv2.minAreaRect(c)
+            # rospy.loginfo(cv2.contourArea(c))
+            box = cv2.boxPoints(box)
+            try:
+                # only proceed if the radius meets a minimum size
+                #rospy.loginfo(index % 10)
+                if radius > 10:
+                    (p1, p2, p3, p4) = box
+                    # width = abs(p1[0] - p2[0])
+                    width = max(abs(p1[0] - p2[0]), abs(p1[0] - p3[0]))
+                    # rospy.loginfo(width)
+                    for (x, y) in box:
+            		          cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+                    #rospy.loginfo(width)
+                    # draw the circle and centroid on the frame,
+                    # then update the list of tracked points
+                    cv2.circle(frame, center, int(radius),
+                               (0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (255, 0, 255), -1)
+                    ballDistanceInfo(color+"--"+str(x)+"--"+str(y)+"--"+str(width))
+                else:
+                    ballDistanceInfo(color+"--"+str(-1)+"--"+str(-1)+"--"+str(-1))
+            except ZeroDivisionError:
+                pass
+    else:
+        ballDistanceInfo(color+"--"+str(-1)+"--"+str(-1)+"---1")
+    return
+
+def draw_helper_lines(frame):
+    # Threshold lines - light blue
+    cv2.line(frame, (threshold_x1, 0), (threshold_x1, cam_height), (255,255,0))
+    cv2.line(frame, (threshold_x2, 0), (threshold_x2, cam_height), (255,255,0))
+    # Lowest center of basket - purple
+    cv2.line(frame, (0, basket_threshold_low), (cam_width, basket_threshold_low), (255, 0, 255))
+    # Ball thresholds - yellow
+    cv2.line(frame, (0, ball_threshold_low), (cam_width, ball_threshold_low), (0, 255, 255))
+    cv2.line(frame, (0, ball_threshold_high), (cam_width, ball_threshold_high), (0, 255, 255))
 
 while not rospy.is_shutdown():
     # Take each frame
@@ -59,12 +154,23 @@ while not rospy.is_shutdown():
 
     #lower_orange = np.array([5,100,140])
     #upper_orange = np.array([15,190,255])
+    # basket blue
+    lower_blue = np.array([80, 0, 20])
+    upper_blue = np.array([100, 255, 150])
 
-    lower_orange = np.array([90, 190, 20])
-    upper_orange = np.array([180, 255, 255])
 
-    lower_green = np.array([40, 80, 70])
-    upper_green = np.array([85, 250, 255])
+    # basket magenta
+    # lower_orange = np.array([0, 100, 75])
+    # upper_orange = np.array([25, 255, 150])
+    #
+    # # basket magenta 2
+    # lower_magenta2 = np.array([155, 100, 150])
+    # upper_magenta2 = np.array([180, 255, 240])
+
+
+    # ball
+    lower_green = np.array([40, 150, 60])
+    upper_green = np.array([80, 255, 255])
 
     #HSV Color
     #lower_green = np.array([70, 100, 100])
@@ -72,19 +178,36 @@ while not rospy.is_shutdown():
 
 
     # Threshold the HSV image to get only blue colors
-    mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    # mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
     mask_green = cv2.inRange(hsv, lower_green, upper_green)
-    #mask_orange = cv2.erode(mask_orange, None, iterations=1)
-    #mask_orange = cv2.dilate(mask_orange, None, iterations=1)
-    mask = mask_green + mask_orange
+    # mask_magenta2 = cv2.inRange(hsv, lower_magenta2, upper_magenta2)
+    # mask_orange = cv2.erode(mask_orange, None, iterations=1)
+    # mask_orange = cv2.dilate(mask_orange, None, iterations=1)
+    # mask_green = cv2.erode(mask_green, None, iterations=2)
+    # mask_green = cv2.dilate(mask_green, None, iterations=2)
 
-    distanceBalls("green", mask_green, frame)
-    distanceBalls("orange", mask_orange, frame)
+    mask_basket = mask_blue
+    mask_ball = mask_green
+    # mask_basket = mask_orange + mask_magenta2
+
+    mask_ball = cv2.erode(mask_ball, None, iterations=2)
+    mask_ball = cv2.dilate(mask_ball, None, iterations=1)
+
+    mask_basket = cv2.erode(mask_basket, None, iterations=3)
+    mask_basket = cv2.dilate(mask_basket, None, iterations=4)
+
+    mask = mask_basket
+
+    distanceBalls("green", mask_ball, frame)
+    distanceBaskets("orange", mask_basket, frame)
+    draw_helper_lines(frame)
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame, frame, mask=mask)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask_orange)
     #cv2.imshow('mask', res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
